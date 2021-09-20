996ab163bd92e6c3c9b8400c9266fc00c46bdfff
Katherine1110 <k.mihalochkina@gmail.com>
2021-09-20 17:58:23 +0300

    Updates

--- styles.6145e9cd.js
+++ styles.6145e9cd.js
@@ -189,100 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"index.js":[function(require,module,exports) {
-"use strict";
-
-require("./styles");
-
-var refs = {
-  listItemCash: document.querySelector('.list-item.cash'),
-  listItemCard: document.querySelector('.list-item.card'),
-  inputCash: document.querySelector('#cash'),
-  inputCard: document.querySelector('#card'),
-  listItemTakeAway: document.querySelector('.list-item.delivery-type-takeAway'),
-  listItemCourier: document.querySelector('.list-item.delivery-type-courier'),
-  listItemNewPostDepartment: document.querySelector('.list-item.delivery-type-newPostDepartment'),
-  listItemNewPostCourier: document.querySelector('.list-item.delivery-type-newPostCourier'),
-  inputTakeAway: document.querySelector('#takeAway'),
-  inputCourier: document.querySelector('#courier'),
-  inputNewPostDepartment: document.querySelector('#newPostDepartment'),
-  inputNewPostCourier: document.querySelector('#newPostCourier'),
-  validationForm: document.querySelector('.validation-form'),
-  submitBtn: document.querySelector('.submitBtn'),
-  formInput: document.querySelectorAll('.form-input')
-};
-refs.inputCash.addEventListener('change', onInputPaymentChange);
-refs.inputCard.addEventListener('change', onInputPaymentChange);
-refs.inputNewPostDepartment.addEventListener('change', onInputDeliveryChange);
-refs.inputNewPostCourier.addEventListener('change', onInputDeliveryChange);
-refs.inputCourier.addEventListener('change', onInputDeliveryChange);
-refs.inputTakeAway.addEventListener('change', onInputDeliveryChange);
-refs.validationForm.addEventListener('submit', onSubmitBtnClick);
-
-function onInputPaymentChange(event) {
-  var target = event.target;
-
-  switch (target.id) {
-    case 'cash':
-      refs.listItemCash.classList.add('chosen');
-      refs.listItemCard.classList.remove('chosen');
-      break;
-
-    case 'card':
-      refs.listItemCard.classList.toggle('chosen');
-      refs.listItemCash.classList.remove('chosen');
-      break;
-  }
-
-  console.log(target);
-}
-
-function onInputDeliveryChange(event) {
-  var target = event.target;
-
-  switch (target.id) {
-    case 'newPostDepartment':
-      refs.listItemNewPostDepartment.classList.add('chosen');
-      refs.listItemNewPostCourier.classList.remove('chosen');
-      refs.listItemCourier.classList.remove('chosen');
-      refs.listItemTakeAway.classList.remove('chosen');
-      break;
-
-    case 'newPostCourier':
-      refs.listItemNewPostCourier.classList.add('chosen');
-      refs.listItemNewPostDepartment.classList.remove('chosen');
-      refs.listItemCourier.classList.remove('chosen');
-      refs.listItemTakeAway.classList.remove('chosen');
-      break;
-
-    case 'courier':
-      refs.listItemCourier.classList.add('chosen');
-      refs.listItemTakeAway.classList.remove('chosen');
-      refs.listItemNewPostDepartment.classList.remove('chosen');
-      refs.listItemNewPostCourier.classList.remove('chosen');
-      break;
-
-    case 'takeAway':
-      refs.listItemTakeAway.classList.add('chosen');
-      refs.listItemCourier.classList.remove('chosen');
-      refs.listItemNewPostDepartment.classList.remove('chosen');
-      refs.listItemCourier.classList.remove('chosen');
-      break;
-  }
-}
-
-function onSubmitBtnClick(event) {
-  // event.preventDefault()
-  for (var i = 0; i < refs.formInput.length; i++) {
-    if (!refs.formInput[i].value) {
-      refs.formInput[i].setAttribute("placeholder", 'Это поле должно быть заполнено');
-      refs.formInput[i].style.borderColor = "red";
-    }
-  }
-
-  return;
-}
-},{"./styles":"styles/index.css"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -486,5 +393,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","index.js"], null)
-//# sourceMappingURL=/src.e31bb0bc.js.map
+},{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
+//# sourceMappingURL=/styles.6145e9cd.js.map
